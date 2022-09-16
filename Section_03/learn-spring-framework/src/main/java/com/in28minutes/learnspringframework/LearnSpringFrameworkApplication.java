6f9769ca56ef5be4cc843338c4b5c5dc77d57858
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-17 00:41:51 +0300

    S3: Step 06 - Iteration 3 - Loose Coupling Level 2 - Spring Framework

--- Section_03/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/LearnSpringFrameworkApplication.java
+++ Section_03/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/LearnSpringFrameworkApplication.java
@@ -2,8 +2,10 @@ package com.in28minutes.learnspringframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GamingConsole;
 import com.in28minutes.learnspringframework.game.MarioGame;
 import com.in28minutes.learnspringframework.game.SuperContraGame;
 
@@ -11,12 +13,14 @@ import com.in28minutes.learnspringframework.game.SuperContraGame;
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(LearnSpringFrameworkApplication.class, args);
 		
-		MarioGame game = new MarioGame();
+		GameRunner runner = context.getBean(GameRunner.class);
+		
+//		MarioGame game = new MarioGame();
 //		SuperContraGame game = new SuperContraGame();
 		
-		GameRunner runner = new GameRunner(game);
+//		GameRunner runner = new GameRunner(game);
 		
 		runner.runGame();
 		
--- Section_03/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
+++ Section_03/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -1,7 +1,12 @@
 package com.in28minutes.learnspringframework.game;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class GameRunner {
 
+	@Autowired
 	private GamingConsole game;
 
 	
