34b1f79ef3b99a8437d44eb0294816e2f10fee1f
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-17 00:48:12 +0300

    S3: Step 07 - Q1 - Spring Framework - Understanding what's happending in the Background

--- Section_03/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/LearnSpringFrameworkApplication.java
+++ Section_03/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/LearnSpringFrameworkApplication.java
@@ -5,22 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
-import com.in28minutes.learnspringframework.game.GamingConsole;
-import com.in28minutes.learnspringframework.game.MarioGame;
-import com.in28minutes.learnspringframework.game.SuperContraGame;
 
 @SpringBootApplication
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		ConfigurableApplicationContext context = 
+				SpringApplication.run(LearnSpringFrameworkApplication.class, args);
 		
 		GameRunner runner = context.getBean(GameRunner.class);
-		
-//		MarioGame game = new MarioGame();
-//		SuperContraGame game = new SuperContraGame();
-		
-//		GameRunner runner = new GameRunner(game);
 		
 		runner.runGame();
 		
