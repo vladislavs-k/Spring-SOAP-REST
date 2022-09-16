c22e0e5513404ceef66f22bd47659df29416926e
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-17 00:58:41 +0300

    S3: Step 09 - Q3 - 01 - Spring Framework - Does Spring Framework Add Value

--- Section_03/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/LearnSpringFrameworkApplication.java
+++ Section_03/learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/LearnSpringFrameworkApplication.java
@@ -3,22 +3,21 @@ package com.in28minutes.learnspringframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 
 @SpringBootApplication
-//@ComponentScan("com.in28minutes.learnspringframework")
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
+		
 		ConfigurableApplicationContext context = 
 				SpringApplication.run(LearnSpringFrameworkApplication.class, args);
 		
 		GameRunner runner = context.getBean(GameRunner.class);
 		
 		runner.runGame();
-		
+
 	}
 
 }
