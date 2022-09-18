c3b9bcadddbe798bb876bad8e16032833955b4bd
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-19 00:44:05 +0300

    S5: Step 09 - Spring Web Services Configuration - Message Dispatcher Servlet

---
+++ Section_05/soap-course-management/src/main/java/com/in28minutes/soap/webservices/soapcoursemanagement/soap/WebServiceConfig.java
@@ -0,0 +1,24 @@
+package com.in28minutes.soap.webservices.soapcoursemanagement.soap;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+
+@EnableWs
+@Configuration
+public class WebServiceConfig {
+	
+	@Bean
+	ServletRegistrationBean messageDisppatcerServlet(ApplicationContext context) {
+		MessageDispatcherServlet messageDispatcherServlet = new MessageDispatcherServlet();
+		messageDispatcherServlet.setApplicationContext(context);
+		messageDispatcherServlet.setTransformWsdlLocations(true);
+		
+		return new ServletRegistrationBean(messageDispatcherServlet, "/ws/*");
+	}
+	
+
+}
