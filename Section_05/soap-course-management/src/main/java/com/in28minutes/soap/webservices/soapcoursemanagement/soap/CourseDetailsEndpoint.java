943c6ce5994370448e7d81d528741e50145e4a3b
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-19 00:31:42 +0300

    S5: Step 08 - Configuring an Endpoint for GetCourseDetailsRequest

---
+++ Section_05/soap-course-management/src/main/java/com/in28minutes/soap/webservices/soapcoursemanagement/soap/CourseDetailsEndpoint.java
@@ -0,0 +1,29 @@
+package com.in28minutes.soap.webservices.soapcoursemanagement.soap;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.in28minutes.courses.CourseDetails;
+import com.in28minutes.courses.GetCourseDetailsRequest;
+import com.in28minutes.courses.GetCourseDetailsResponse;
+
+@Endpoint
+
+public class CourseDetailsEndpoint {
+
+	@PayloadRoot(namespace = "http://in28minutes.com/courses", localPart = "GetCourseDetailsRequest")
+	@ResponsePayload
+	public GetCourseDetailsResponse 
+		processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
+		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		CourseDetails courseDetails = new CourseDetails();
+		courseDetails.setId(request.getId());
+		courseDetails.setName("Microservices");
+		courseDetails.setDescription("Great course!");
+
+		return response;
+	}
+
+}
