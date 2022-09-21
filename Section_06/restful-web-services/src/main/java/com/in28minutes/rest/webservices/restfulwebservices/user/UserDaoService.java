e48a1fa3f9c9ccc246ef0dc64faad92ad0c01593
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-21 04:06:13 +0300

    S6: Step 08 - Implementing GET Methods for User Resource

--- Section_06/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ Section_06/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -3,6 +3,7 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Component;
 
@@ -21,5 +22,8 @@ public class UserDaoService {
 		return users;
 	}
 	
-	
+	public User findOne(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		return users.stream().filter(predicate).findFirst().get();
+	}
 }
