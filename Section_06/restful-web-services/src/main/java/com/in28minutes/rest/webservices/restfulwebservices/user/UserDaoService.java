6b2216fb2d6774f3a9d1b9a873b15ffeb8902baa
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-21 04:20:41 +0300

    S6: Step 09 - Implementing POST Method to create User Resource

--- Section_06/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ Section_06/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -11,12 +11,15 @@ import org.springframework.stereotype.Component;
 public class UserDaoService {
 	
 	private static List<User> users = new ArrayList<>();
+	private static int usersCount = 0;
 	
 	static {
-		users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
-		users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
-		users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+		users.add(new User(++usersCount, "Adam", LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount, "Eve", LocalDate.now().minusYears(25)));
+		users.add(new User(++usersCount, "Jim", LocalDate.now().minusYears(20)));
 	}
+	
+	
 	
 	public List<User> findAll() {
 		return users;
@@ -25,5 +28,11 @@ public class UserDaoService {
 	public User findOne(int id) {
 		Predicate<? super User> predicate = user -> user.getId().equals(id);
 		return users.stream().filter(predicate).findFirst().get();
+	}
+	
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
 	}
 }
