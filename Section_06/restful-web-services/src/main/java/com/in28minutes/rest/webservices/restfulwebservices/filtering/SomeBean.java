d6a182a7aa592f3353dba2b4e97731a7be5ff370
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-25 01:22:53 +0300

    S6: Step 24 - Implementing Dynamic Filtering for REST API

--- Section_06/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ Section_06/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,10 +1,10 @@
 package com.in28minutes.rest.webservices.restfulwebservices.filtering;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonFilter;
 
 //@JsonIgnoreProperties("field1")
-@JsonIgnoreProperties({"field1", "field3"})
+//@JsonIgnoreProperties({"field1", "field3"})
+@JsonFilter(value = "SomeBeanFilter")
 public class SomeBean {
 	
 	private String field1;
