497740b2f775eb8c0ad4f97dc9cec4db53273c13
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-25 04:51:27 +0300

    S7: Step 04 - Getting Started with Spring JDBC

---
+++ Section_07/learn-jpa-and-hibernate/sql-commands.sql
@@ -0,0 +1,6 @@
+INSERT INTO course VALUES (1, 'Learn AWS', 'in28minutes');
+INSERT INTO course (id, name, author) VALUES (1, 'Learn AWS', 'in28minutes');
+
+SELECT * FROM course;
+
+DELETE FROM course WHERE id = 1; 
