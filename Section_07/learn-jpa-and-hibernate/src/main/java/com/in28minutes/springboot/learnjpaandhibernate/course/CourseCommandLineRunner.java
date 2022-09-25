61ee0e67f72efe835f6f208a685e3f5c848b60d9
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-25 06:17:53 +0300

    S7: Step 10 - Getting Started with Spring Data JPA

--- Section_07/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ Section_07/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner{
@@ -12,19 +13,25 @@ public class CourseCommandLineRunner implements CommandLineRunner{
 //	@Autowired
 //	private CourseJdbcRepository repository;
 	
+//	@Autowired
+//	private CourseJpaRepository repository;
+	
 	@Autowired
-	private CourseJpaRepository repository;
+	private CourseSpringDataJpaRepository repository;
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert(new Course(1, "JPS Learn AWS Now!", "in28miutes"));
-		repository.insert(new Course(2, "JPS Learn Azure Now!", "in28miutes"));
-		repository.insert(new Course(3, "JPS Learn DevOps Now!", "in28miutes"));
+//		repository.insert(new Course(1, "JPS Learn AWS Now!", "in28miutes"));
+//		repository.insert(new Course(2, "JPS Learn Azure Now!", "in28miutes"));
+//		repository.insert(new Course(3, "JPS Learn DevOps Now!", "in28miutes"));
+		repository.save(new Course(1, "Spring Data JPA Learn AWS Now!", "in28miutes"));
+		repository.save(new Course(2, "Spring Data JPA Learn Azure Now!", "in28miutes"));
+		repository.save(new Course(3, "Spring Data JPA Learn DevOps Now!", "in28miutes"));
 		
-		repository.deleteById(1);
+		repository.deleteById(1l);
 		
-		System.out.println(repository.findById(2));
-		System.out.println(repository.findById(3));
+		System.out.println(repository.findById(2l));
+		System.out.println(repository.findById(3l));
 	}
 
 }
