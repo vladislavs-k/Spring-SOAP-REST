78e78b8c5040e8802e780bf4a030b71ced22b8a5
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-26 01:18:13 +0300

    S8: Step 28 - Creating User Entity and some test data

---
+++ Section_06/restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO user_details (id, birth_date, name) 
+VALUES (10001, current_date(), 'Vlad');
+
+INSERT INTO user_details (id, birth_date, name) 
+VALUES (10002, current_date(), 'Bob');
+
+INSERT INTO user_details (id, birth_date, name) 
+VALUES (10003, current_date(), 'Jane');
