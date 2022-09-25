2bb51f4f93da1c106f4c8cb626dfb62318c4c88c
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-25 06:34:06 +0300

    S7: Step 11 - Exploring features of Spring Data JPA

--- Section_07/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ Section_07/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -21,17 +21,28 @@ public class CourseCommandLineRunner implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {
-//		repository.insert(new Course(1, "JPS Learn AWS Now!", "in28miutes"));
-//		repository.insert(new Course(2, "JPS Learn Azure Now!", "in28miutes"));
-//		repository.insert(new Course(3, "JPS Learn DevOps Now!", "in28miutes"));
-		repository.save(new Course(1, "Spring Data JPA Learn AWS Now!", "in28miutes"));
-		repository.save(new Course(2, "Spring Data JPA Learn Azure Now!", "in28miutes"));
-		repository.save(new Course(3, "Spring Data JPA Learn DevOps Now!", "in28miutes"));
+//		repository.insert(new Course(1, "JPS Learn AWS Now!", "in28minutes"));
+//		repository.insert(new Course(2, "JPS Learn Azure Now!", "in28minutes"));
+//		repository.insert(new Course(3, "JPS Learn DevOps Now!", "in28minutes"));
+		repository.save(new Course(1, "Spring Data JPA Learn AWS Now!", "in28minutes"));
+		repository.save(new Course(2, "Spring Data JPA Learn Azure Now!", "in28minutes"));
+		repository.save(new Course(3, "Spring Data JPA Learn DevOps Now!", "in28minutes"));
 		
 		repository.deleteById(1l);
 		
 		System.out.println(repository.findById(2l));
 		System.out.println(repository.findById(3l));
+		
+		System.out.println("=========================================================");
+		System.out.println(repository.findAll());
+		System.out.println();
+		System.out.println(repository.count());
+		System.out.println();
+		System.out.println(repository.findByAuthor("in28minutes"));
+		System.out.println(repository.findByAuthor(""));
+		System.out.println();
+		System.out.println(repository.findByName("pring Data JPA Learn AWS Now!"));
+		System.out.println(repository.findByName("Spring Data JPA Learn DevOps Now!"));
 	}
 
 }
