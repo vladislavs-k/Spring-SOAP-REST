d5054a5fc426a14ca22be9f839fb8f3b77db91a8
Vladislavs K <vladislavs.kcs@gmail.com>
2022-09-26 23:28:02 +0300

    S11: Step 03 - Improving Java Functional Program with filter

--- Section_11/functional-programming-with-java/src/programming/FP01Functional.java
+++ Section_11/functional-programming-with-java/src/programming/FP01Functional.java
@@ -1,26 +1,38 @@
 package programming;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class FP01Functional {
 
 	public static void main(String[] args) {
 
-		printAllNumbersInListFunctional(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
+		List<Integer> Numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+//		printAllNumbersInListFunctional(Numbers);
+		printEvenNumbersInListFunctional(Numbers);
 	}
 	
-	private static void print(int number) {
-		System.out.println(number);
-	}
+//	private static void print(int number) {
+//		System.out.println(number);
+//	}
 
 	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
-
-//		for(int number : numbers) {
-//			System.out.println(number);
-//		}
 		
-		System.out.println("Functional");
-		numbers.stream().forEach(FP01Functional::print);	// :: - Method Reference
+		System.out.println("Functional:");
+//		numbers.stream().forEach(FP01Functional::print);	// :: - Method Reference
+		numbers.stream().forEach(System.out::println);
 	}
+	
+	
+	private static boolean isEven(int number) {
+		return number % 2 == 0;
+	}
+	
+	private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+		
+		System.out.println("Functional Even: ");
+		numbers.stream().filter(FP01Functional::isEven).forEach(System.out::println);
+	}
+	
 
 }
--- Section_11/functional-programming-with-java/src/programming/FP01Structured.java
+++ Section_11/functional-programming-with-java/src/programming/FP01Structured.java
@@ -6,14 +6,25 @@ public class FP01Structured {
 
 	public static void main(String[] args) {
 
-		printAllNumbersInListStructured(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
+		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+//		printAllNumbersInListStructured(numbers);
+		printEvenNumbersInListStructured(numbers);
 	}
 
 	private static void printAllNumbersInListStructured(List<Integer> numbers) {
 
-		System.out.println("Structured");
-		for(int number : numbers) {
+		System.out.println("Structured:");
+		for (int number : numbers) {
 			System.out.println(number);
+		}
+	}
+
+	private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+
+		System.out.println("Structured Even:");
+		for (int number : numbers) {
+			if (number % 2 == 0)
+				System.out.println(number);
 		}
 	}
 
